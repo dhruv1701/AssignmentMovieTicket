436b5cbfa96c20d1cbec0f9380b7ff6ae942a895
dhruv sharma <sharmadhruv17011999@gmail.com>
2021-03-19 13:21:41 +0530

    added comment in ticket repo to delete ticket

--- src/main/java/com/example/AssignmentMovieTicket/Repository/ticketRepository.java
+++ src/main/java/com/example/AssignmentMovieTicket/Repository/ticketRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ticketRepository extends JpaRepository<ticket, Long> {
 //    findAllByOwner(users user)
 //    findAllByShows(movieShows show)
+//    void deleteByShowsShowDateTimeAfter(Date expiryDate);
 }
